c35689c385d05b92fc060d9a95287f8651f32085
ERROR-404 <arvinfranceliso071699@gmail.com>
2022-06-04 22:57:32 +0800

    Add project files.

---
+++ aspnet-core/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using Acme.BookStore.Books;
+using AutoMapper;
+using Acme.BookStore.Authors;
+
+namespace Acme.BookStore
+{
+    public class BookStoreApplicationAutoMapperProfile : Profile
+    {
+        public BookStoreApplicationAutoMapperProfile()
+        {
+            CreateMap<Book, BookDto>();
+            CreateMap<CreateUpdateBookDto, Book>();
+            CreateMap<Author, AuthorDto>();
+            CreateMap<Author, AuthorLookupDto>();
+        }
+    }
+}
---
+++ aspnet-core/src/Acme.BookStore.IdentityServer/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace Acme.BookStore.Pages;
+
+public class IndexModel : AbpPageModel
+{
+    public void OnGet()
+    {
+    }
+}
